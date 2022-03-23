677a8c218302646989920395d7e2c6958a567139
Zach Gephart <gephartzachary@gmail.com>
2022-03-22 23:14:16 -0500

    FINAL

--- lab_6/map_lab6.js
+++ lab_6/map_lab6.js
@@ -101,7 +101,7 @@ function getColor2(value) {
 
 function style2(feature){
     return {
-        fillColor: getColor(feature.properties.lan_den),
+        fillColor: getColor2(feature.properties.lan_den),
         weight: 2,
         opacity: 1,
         color: 'gray',
@@ -141,7 +141,7 @@ geojson2 = L.geoJson(data, {
     style: style,
     onEachFeature: onEachFeature
 }).bindPopup(function (layer){
-return layer.feature.properties.NAME 
+    return layer.feature.properties.NAME 
         + '<p style="color:red">' + layer.feature.properties.lan_den.toString() + ' people/hectare </p>';       
 }).addTo(mymap);
 
@@ -150,15 +150,15 @@ var legend2 = L.control({position: 'bottomright'});
 legend2.onAdd = function (mymap) {
 
     var div = L.DomUtil.create('div', 'legend'),
-        grades = [0, 14, 22, 35, 47];
+        grades = [0, 15, 23, 36, 48];
 
-    div.innerHTML = '<b>Population Density <br></b>';
+    div.innerHTML = '<b>All English Density <br></b>';
     
     // Loop through our the classes and generate a label with a color box for each interval.
     // If you are creating a choropleth map, you DO NOT need to change lines below.
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
-        '<i style="background:' + getColor(grades[i] + 1) + '"></i>' +
+        '<i style="background:' + getColor2(grades[i] + 1) + '"></i>' +
         grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
 
@@ -169,9 +169,9 @@ legend2.addTo(mymap);
 
 // ~~~~~~~ Menu ~~~~~~~~~~
 
-let layers = {
+var mapLayers = {
     "Population" : geojson,
     "Language" : geojson2,
 };
 
-var layerControl = L.control.layers(mapLayers).addTo(mymap);
+var layerControl = L.control.layers(mapLayers, {collapsed: false}).addTo(mymap);
