0b9046e215276fab318522722b658d558f20d0e8
Zach Gephart <gephartzachary@gmail.com>
2022-04-28 11:48:34 -0500

    ommit

---
+++ final/locations.js
@@ -0,0 +1,300 @@
+var locations = {
+    "type": "FeatureCollection",
+    "name": "mtn_peaks_himalayas",
+    "coordinates": {
+        "type": "name",
+        "properties": {
+            "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+        }
+    },
+    "features": [
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Centre interarmees d'essais d'engis speciaux",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 1947,
+            "year_end": 1967,
+            "num_launches": 230,
+            "highest": "Orbital"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [31.09951, 2.83581]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Broglio Space Centre",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 1964,
+            "year_end": 1988,
+            "num_launches": 27,
+            "highest": "Orbital"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-2.94080, 40.21340]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Jiuquan Satellite Launch Center",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 1970,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Orbital"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [40.96056, 100.29833]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Taiyuan Satellite Launch Center",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 1980,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Orbital"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [39.14321, 111.96741]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Xichang Satellite Launch Center",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 1984,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Lunar"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [40.96056, 100.29833]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Wenchang Satellite Launch Center",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 2016,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Interplanetary"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [19.614492, 110.951133]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Satish Dhawn Space Centre",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 1971,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Interplanetary"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [13.73740, 80.23510]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Semnan Spaceport",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 2009,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Orbital"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [35.234631, 53.920941]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Palmachim Air Force Base",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 1987,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Orbital"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [31.88484, 34.68020]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Uchinoura Space Center",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 1962,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Interplanetary"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [31.25186, 131.07914]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Tanegashima Space Center",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 1967,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Interplanetary"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [30.39096, 130.96813]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Baikonur Cosmodrome",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 1955,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Interplanetary"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [45.95515, 63.35028]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Naro Space Center",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 2008,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Orbital"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [34.42585, 127.52793]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Svobodny Cosmodrome",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 1957,
+            "year_end": 2007,
+            "num_launches": null,
+            "highest": "Orbital"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [51.83441, 128.27570]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "Yasny Cosmodrome",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": 2006,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Orbital"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [51.20706, 59.85003]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "TITLE": "",
+            "VISIBLE": 1,
+            "DESCRIPTIO": null,
+            "DATE": null,
+            "TYPEID": 0,
+            "year_start": ,
+            "year_end": null,
+            "num_launches": null,
+            "highest": "Orbital"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": []
+        }
+    },
+    ]
+};
